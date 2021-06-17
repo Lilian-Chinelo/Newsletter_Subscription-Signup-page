067cba62121b8640e13c02440f5be3bf55ccceaa
Lilian Umeakunne <73794416+Lilian-Chinelo@users.noreply.github.com>
2021-06-17 20:42:15 +0100

    Update app.js
    Updated a few changes for authentication.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const { post } = require("request");
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static("public")); //specifies a static folder with files used in the project
+app.use(express.static("public"));
 
 app.get("/", function(req , res){
     res.sendFile(__dirname + "/signup.html");
@@ -21,7 +21,7 @@ app.post("/", function(req, res){
     const Email = req.body.email;
 
     const data = {
-        members: [          //The process for parsing data correctly to mailchimp
+        members: [          
             {
                 email_address: Email,
                 status: "subscribed",
@@ -35,36 +35,34 @@ app.post("/", function(req, res){
 
     const jsonData = JSON.stringify(data);
 
-    const url = "https://us6.api.mailchimp.com/3.0/lists/64687fc0e0"; //mailchimp list ID at the end of the end point + list
-
-    const options = {
+    const url = "https://us6.api.mailchimp.com/3.0/lists/64687fc0e0";
         method: "POST",
-        auth: "LilianU:58d8b69e1d474687c696a8aac16b34f6-us6" //API Key + username for authentication
+        auth: "LilianU:APIKey"
     }
 
     const request = https.request(url, options, function(response){
 
         if (response.statusCode === 200){
-            res.sendFile(__dirname + "/success.html"); //sends the sucess page to the user
+            res.sendFile(__dirname + "/success.html");
 
         } else{
-           res.sendFile(__dirname + "/failure.html"); //sends the failure page to the user
+           res.sendFile(__dirname + "/failure.html");
         }
         response.on("data", function(data){
-            console.log(JSON.parse(data)); //parse data as Json file
+            console.log(JSON.parse(data));
         })
     })
 
     request.write(jsonData);
-    request.end(); //indicates end of request
+    request.end();
 
     app.post("/failure", function(req , res){
-        res.redirect("/"); //redirects the user to the home root to try again
+        res.redirect("/");
     })
 });
 
 
 
-app.listen(process.env.PORT || 3000, function(req , res){ //the process.env.PORT allows the host server to auto asign a port. 3000 allows for local listening still if needed.
+app.listen(process.env.PORT || 3000, function(req , res){
     console.log("Serve is now running on port 3000");
 });
